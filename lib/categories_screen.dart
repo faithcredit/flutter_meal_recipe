85c04d6ce6d1c5f15ab53649fcf14e5c7c73c754
golangdevm <honest.dedicate@gmail.com>
2023-06-14 21:47:40 -0700

    Styling and themes

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DeliMeals',
-      theme: ThemeData(
-   
-        primarySwatch: Colors.blue,
+  title: 'DeliMeals',
+  theme: ThemeData(
+    colorScheme: ColorScheme.fromSwatch(
+      primarySwatch: Colors.pink,
+      accentColor: Colors.amber,
+    ),
+    canvasColor: Color.fromRGBO(255, 254, 229, 1),
+    fontFamily: 'Raleway',
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(
+        color: Color.fromRGBO(20, 51, 51, 1),
       ),
-      home: CategoriesScreen(),
-    );
+      bodyText2: TextStyle(
+        color: Color.fromRGBO(20, 51, 51, 1),
+      ),
+      headline6: TextStyle(
+        fontSize: 20,
+        fontFamily: 'RobotoCondensed',
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  ),
+  home: CategoriesScreen(),
+);
   }
 }
