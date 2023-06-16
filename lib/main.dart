a5cfc0abca4fcdbe7b5653d9a18281890ab52e22
golangdevm <honest.dedicate@gmail.com>
2023-06-16 16:37:32 -0700

    adding filter logic

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,71 @@ import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/tabs_screen.dart';
+import './models/meal.dart';
+import './dummy_data.dart';
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+ Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  // List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters =filterData;
+
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters['gluten']! && !meal.isGlutenFree!) {
+          return false;
+        }
+        if (_filters['lactose']! && !meal.isLactoseFree!) {
+          return false;
+        }
+        if (_filters['vegan']! && !meal.isVegan!) {
+          return false;
+        }
+        if (_filters['vegetarian']! && !meal.isVegetarian!) {
+          return false;
+        }
+        return true;
+      }).toList();
+
+    });
+  }
+
+  // void _toggleFavorite(String mealId) {
+  //   final existingIndex =
+  //       _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+  //   if (existingIndex >= 0) {
+  //     setState(() {
+  //       _favoriteMeals.removeAt(existingIndex);
+  //     });
+  //   } else {
+  //     setState(() {
+  //       _favoriteMeals.add(
+  //         DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+  //       );
+  //     });
+  //   }
+  // }
+
+  // bool _isMealFavorite(String id) {
+  //   return _favoriteMeals.any((meal) => meal.id == id);
+  // }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -40,9 +98,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/', // default is '/'
       routes: {
         '/': (ctx) => TabsScreen(),
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(_availableMeals),
         MealDetailScreen.routeName:(ctx) => MealDetailScreen(), 
-        FiltersScreen.routeName: (ctx) => FiltersScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters,_setFilters),
       },
 
       onGenerateRoute:(settings) {
--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import '../widgets/meal_item.dart';
-import '../dummy_data.dart';
 import '../models/meal.dart';
 
 class CategoryMealsScreen extends StatefulWidget {
   static const routeName = '/category-meals';
 
+final List<Meal> availableMeals;
+
+CategoryMealsScreen(this.availableMeals);
   @override
   State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
 }
@@ -29,7 +31,7 @@ class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
           ModalRoute.of(context)!.settings.arguments as Map<String, String>;
       categoryTitle = routeArgs['title'];
       final categoryId = routeArgs['id'];
-      displayedMeals = DUMMY_MEALS.where((meal) {
+      displayedMeals = widget.availableMeals.where((meal) {
         return meal.categories!.contains(categoryId);
       }).toList();
       _loadedInitData = true;
